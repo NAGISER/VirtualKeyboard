8fbcb42ce808372d7a9e3970a70721a252c6fbb0
unknown <pasha.shamanov10@mail.ru>
2026-05-07 17:01:30 +0300

    Only archive RAR

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Keyboard.h"
-
-int main(int argc, char* argv[]) {
-	setlocale(LC_ALL,"RUS");
-	auto app = Gtk::Application::create("org.example.keyboard");
-	Keyboard keyboard;
-	keyboard.run(app);
-	return 0;
-}
